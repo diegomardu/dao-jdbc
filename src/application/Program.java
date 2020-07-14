c99746086ca90981e8ba92740e509041ce727cf8
diegomardu <diegomartins836@gmail.com>
2020-07-14 16:46:02 -0300

    SellerDao e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +13,8 @@ public class Program {
 		Department obj = new Department(1, "Diego");
 		
 		Seller seller = new Seller(21, "Diego", "diego@teste.com", new Date(), 3000.0, obj);
+		
+		SellerDao sallerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
